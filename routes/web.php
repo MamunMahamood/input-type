828b6f8e2916409767aad8ace0323b3ef406c9be
MamunMahamood <golam.mahamood.mamun@gmail.com>
2024-10-30 23:35:13 +0600

    edit-delete-done

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ Route::prefix('user')->group(function () {
     Route::get('/create', [UserController::class, 'create'])->name('user.create');
     Route::post('/store', [UserController::class, 'store'])->name('user.store');
     Route::get('/list', [UserController::class, 'index'])->name('user.list');
+    Route::get('/edit/{id}', [UserController::class, 'edit'])->name('user.edit');
+    Route::post('/update/{id}', [UserController::class, 'update'])->name('user.update');
+    Route::get('/delete/{id}', [UserController::class, 'delete'])->name('user.delete');
 });
 
 
